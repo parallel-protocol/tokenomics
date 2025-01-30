e7207f381caf06e079b95546006de2ab2545c7d2
FabienC <fabiencoutant.ctt@gmail.com>
2025-01-30 12:08:58 +0200

    fix: issue_12 - Users can grief fee transfers

--- contracts/fees/SideChainFeeCollector.sol
+++ contracts/fees/SideChainFeeCollector.sol
@@ -76,7 +76,14 @@ contract SideChainFeeCollector is FeeCollectorCore {
     /// @notice Release the fee token to the MainFeeDistributor on the receiving chain.
     /// @param _options Options to be passed to the bridgeable token.
     /// @return amountSent The amount of fee token that has been bridged.
-    function release(bytes memory _options) external payable nonReentrant whenNotPaused returns (uint256 amountSent) {
+    function release(bytes memory _options)
+        external
+        payable
+        nonReentrant
+        whenNotPaused
+        restricted
+        returns (uint256 amountSent)
+    {
         amountSent = _calcBridgeableAmount();
         if (amountSent == 0) {
             revert NothingToRelease();
